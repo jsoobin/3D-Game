6c729c0fd332edf4d66777193dd4685cca30b027
jsoobin <84570147+jsoobin@users.noreply.github.com>
2021-06-12 22:31:48 +0900

    Create Crosshair.cs

---
+++ Crosshair.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crosshair : MonoBehaviour
+{
+
+    [SerializeField]
+    private Animator animator;
+
+    private float gunAccuracy;
+
+    [SerializeField]
+    private GameObject go_CrosshairHUD;
+    [SerializeField]
+    private GunController theGunController;
+
+    public void WalkingAnimation(bool _flag)
+    { if (!GameManager.isWater)
+        {
+            animator.SetBool("Walking", _flag);
+
+        }
+    }
+
+    public void RunningAnimation(bool _flag)
+    {
+        if (!GameManager.isWater)
+        {
+            animator.SetBool("Running", _flag);
+        }
+    }
+    public void FireAnimation()
+    {
+        if (!GameManager.isWater)
+        {
+            if (animator.GetBool("Walking"))
+                animator.SetTrigger("Walk_Fire");
+            else
+                animator.SetTrigger("Idle_Fire");
+        }
+    }
+
+    public float GetAccuracy()
+    {
+        {
+            if (animator.GetBool("Walking"))
+                gunAccuracy = 0.06f;
+            else
+                gunAccuracy = 0.035f;
+
+            return gunAccuracy;
+        }
+    }
+}
