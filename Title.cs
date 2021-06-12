81adf5a506d445ca583075c6c0264c97f018a6e3
jsoobin <84570147+jsoobin@users.noreply.github.com>
2021-06-12 22:41:41 +0900

    Create Title.cs

---
+++ Title.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class Title : MonoBehaviour
+{
+
+    public string sceneName = "GameStage";
+
+    public static Title instance;
+
+    private SaveNLoad theSaveNLoad;
+    private void Awake()
+    {
+
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+            Destroy(this.gameObject);
+    }
+
+    public void ClickStart()
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+  
+    IEnumerator LoadCoroutine()
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneName);
+
+        while(!operation.isDone)
+        {
+            yield return null;
+        }
+
+        theSaveNLoad = FindObjectOfType<SaveNLoad>();
+
+        theSaveNLoad.LoadData();
+
+        Destroy(gameObject);
+    }
+
+    public void ClickExit()
+    {
+        Application.Quit();
+    }
+
+
+}
