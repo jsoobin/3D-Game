8e48aee8ca61c30c49c5aac1d8ed0c5c4a1b4aae
jsoobin <84570147+jsoobin@users.noreply.github.com>
2021-06-12 22:31:15 +0900

    Create Boss.cs

---
+++ Boss.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.SceneManagement;
+
+public class Boss : MonoBehaviour
+{
+    public List<Transform> locations;
+    public Transform patrolRoute;
+
+    private int locationIndex = 0;
+    private NavMeshAgent agent;
+    private GameManager gameManager;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+
+        InitializePatrolRoute();
+        MoveToNextPatrolPoint();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (agent.remainingDistance < 0.2f && !agent.pathPending)
+        {
+            MoveToNextPatrolPoint();
+        }
+
+    }
+
+    void InitializePatrolRoute()
+    {
+        foreach (Transform child in patrolRoute)
+        {
+            locations.Add(child);
+        }
+    }
+
+    void MoveToNextPatrolPoint()
+    {
+        agent.destination = locations[locationIndex].position;
+        locationIndex = (locationIndex + 1) % locations.Count;
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+
+        if (collision.gameObject.name == "Player")
+        {
+            SceneManager.LoadScene("GameStage");
+        }
+    }
+
+}
